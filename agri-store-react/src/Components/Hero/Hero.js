655bc0f5aa68330353101b75eaa6b5760eacf49e
iprankurpandey <pprankur@gmail.com>
2022-05-02 21:11:26 +0530

    updated branch

--- agri-store-react/src/Components/Hero/Hero.js
+++ agri-store-react/src/Components/Hero/Hero.js
@@ -3,18 +3,18 @@ import "./Hero.css";
 function Hero() {
   return (
     <div>
-      <main class="main-container">
-        <div class="text-in-image">
-          <span class="mtext">
-            <div class="h1">Agri Store</div>
+      <main className="main-container">
+        <div className="text-in-image">
+          <span className="mtext">
+            <div className="h1">Agri Store</div>
           </span>
-          <span class="head-text"> by farmers ,to farmers. </span>
-
-          <div class="logo-text">Summer Sale 2022</div>
-
-          <div class="subtext-image">All in one Market Place for Farmers</div>
-          <div class="btn btn-outline">
-            <button class="btn btn-success-outline">Shop Now</button>
+          <span className="head-text"> by farmers ,to farmers !</span>
+          <div className="logo-text">Summer Sale 2022</div>
+          <div className="subtext-image">
+            All in one Market Place for Farmers
+          </div>
+          <div className="btn btn-outline">
+            <button className="btn btn-success-outline">Shop Now</button>
           </div>
         </div>
       </main>
