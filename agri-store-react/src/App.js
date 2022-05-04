984460e77c445f9b6d734a07211514c9ff4c08e5
iprankurpandey <pprankur@gmail.com>
2022-05-04 19:48:42 +0530

    fix :  addec context

--- agri-store-react/src/App.js
+++ agri-store-react/src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Homepage from "./Pages/Homepage";
 import LoginPage from "./Pages/LoginPage";
+import ProductlistingPage from "./Pages/ProductlistingPage";
 import SignupPage from "./Pages/SignupPage";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="LoginPage" element={<LoginPage />} />
         <Route path="SignupPage" element={<SignupPage />} />
+        <Route path="ProductListingPage" element={<ProductlistingPage />} />
       </Routes>
     </div>
   );
--- agri-store-react/src/index.js
+++ agri-store-react/src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter } from "react-router-dom";
+import { ProductListingPage } from "./Components/Context/ContextProductListingPage";
+
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ProductListingPage>
+        <App />
+      </ProductListingPage>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
