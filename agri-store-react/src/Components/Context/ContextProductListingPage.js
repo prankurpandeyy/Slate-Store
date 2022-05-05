66b3146b585e1e77baafd93047865e80968ae680
iprankurpandey <pprankur@gmail.com>
2022-05-05 09:53:58 +0530

    FEAT : PRODUCT LISTING PAGE COMPLETED WITH LOADER AND APIs

--- agri-store-react/src/Components/Context/ContextProductListingPage.js
+++ agri-store-react/src/Components/Context/ContextProductListingPage.js
@@ -23,7 +23,7 @@ function ProductListingPage({ children }) {
   useEffect(() => {
     const timer = setTimeout(() => {
       getProdcutsData();
-    }, 5000);
+    }, 2000);
 
     return () => clearTimeout(timer);
   }, []);
