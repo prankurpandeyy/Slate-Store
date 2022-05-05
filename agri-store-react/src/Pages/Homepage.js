a27fedc5e4f04b0c5513902c836bb826fb82252c
iprankurpandey <pprankur@gmail.com>
2022-05-05 09:46:57 +0530

    fix :  updated code

--- agri-store-react/src/Pages/Homepage.js
+++ agri-store-react/src/Pages/Homepage.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Spinner from "../Components/DataLoaderSpinner/Spinner";
 import { FeaturedBrands } from "../Components/FeaturerdBrands/FeaturedBrands";
 import { Footer } from "../Components/Footer/Footer";
 import { Header } from "../Components/Header/Header";
--- agri-store-react/src/Pages/ProductlistingPage.js
+++ agri-store-react/src/Pages/ProductlistingPage.js
@@ -1,16 +1,30 @@
 import React, { useContext } from "react";
+import { ProductListingPagecontext } from "../Components/Context/ContextProductListingPage";
 import { Footer } from "../Components/Footer/Footer";
 import { Header } from "../Components/Header/Header";
+import Spinner from "../Components/LoadingSpinner/Spinner";
 import ProductCard from "../Components/ProductCard/ProductCard";
 import { Sidebar } from "../Components/Sidebar/Sidebar";
 import "./ProductlistingPage.css";
 function ProductlistingPage() {
+  const { productdata, isLoading } = useContext(ProductListingPagecontext);
   return (
     <div>
       <Header />
-      <div className="product-cards">
-        <Sidebar />
-        <ProductCard />
+      <Sidebar />
+      <div className="productlisting-cards">
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          productdata.map((productCardData) => {
+            return (
+              <ProductCard
+                productCardData={productCardData}
+                key={productCardData._id}
+              />
+            );
+          })
+        )}
       </div>
       <Footer />
     </div>
