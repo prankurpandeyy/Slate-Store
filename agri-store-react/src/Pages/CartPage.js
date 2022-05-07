eb55d0079613f09dc67dc92c16e3ba7cbcf268aa
iprankurpandey <pprankur@gmail.com>
2022-05-07 22:40:21 +0530

    fix :  braking table issue

--- agri-store-react/src/Pages/CartPage.js
+++ agri-store-react/src/Pages/CartPage.js
@@ -4,7 +4,15 @@ import { ProductListingPagecontext } from "../Components/Context/ContextProductL
 import { Header } from "../Components/Header/Header";
 import { Footer } from "../Components/Footer/Footer";
 function CartPage() {
-  const { cart, deleteQty, updateQty } = useContext(ProductListingPagecontext);
+  const {
+    cart,
+    deleteQty,
+    updateQty,
+    deleteWishList,
+    addToWishlist,
+    setWish,
+    wish,
+  } = useContext(ProductListingPagecontext);
   const [totalprice, setTotalPrice] = useState();
 
   useEffect(() => {
@@ -16,69 +24,90 @@ function CartPage() {
   return (
     <div>
       <Header cart={cart} />
-      <div className="cart-price-container">
+
+      {cart.length === 0 ? (
+        <div className="cart-mesg"> there are no items in Cart </div>
+      ) : (
         <table id="customers">
           <tr>
-            <th>Name</th>
+            <th>Title</th>
             <th>Image</th>
             <th>Price</th>
-            <th>Quantity</th>
-            <th>Remove</th>
+            <th>Qty</th>
+            <th>Wishlist</th>
+            <th>Delete</th>
           </tr>
-        </table>
 
-        {cart.map((prod) => (
-          <div className="card-item-style">
-            <table>
-              <tr>
-                <div className="card-title">
-                  <td>{prod.title}</td>
+          {cart.map((prod) => (
+            <tr>
+              <td>{prod.title}</td>
+              <td>
+                <img src={prod.image} alt="image_text" />
+              </td>
+              <td>
+                <h3> ₹ {prod.price}</h3>
+              </td>
+              <td>
+                <div className="cart-btn">
+                  <button
+                    class="hcard increase"
+                    onClick={() => updateQty("increment", prod._id)}
+                  >
+                    <span class="material-icons">add</span>
+                  </button>
+                  {prod.qty}
+                  <button
+                    class="hcard decrease"
+                    onClick={() => updateQty("decrement", prod._id)}
+                  >
+                    <span class="material-icons">remove</span>
+                  </button>
                 </div>
-                <td>
-                  <div className="card-image">
-                    <img src={prod.image} alt="logo" />
-                  </div>
-                </td>
-                <td>
-                  <div className="card-price">₹{prod.price}</div>
-                </td>
+              </td>
 
-                <td>
-                  <div class="hcard-btn">
-                    <button
-                      class="hcard increase"
-                      onClick={() => updateQty("increment", prod._id)}
-                    >
-                      <span class="material-icons">add</span>
-                    </button>
-                    {prod.qty}
+              <td>
+                <button
+                  className="hcard decrease"
+                  onClick={(_id) => deleteQty(prod._id)}
+                >
+                  <span class="material-icons">delete</span>
+                </button>
+              </td>
+              <td>
+                <div className="cart-btn">
+                  {wish.some((wishlist) => wishlist._id === prod._id) ? (
                     <button
                       class="hcard decrease"
-                      onClick={() => updateQty("decrement", prod._id)}
+                      onClick={(_id) => deleteWishList(prod._id)}
                     >
-                      <span class="material-icons">remove</span>
+                      <span class="material-icons buttonmi">heart_broken</span>
                     </button>
-                  </div>
-                </td>
-                <td>
-                  <button
-                    className="hcard decrease"
-                    onClick={(_id) => deleteQty(prod._id)}
-                  >
-                    <span class="material-icons">delete</span>
-                  </button>
-                </td>
-              </tr>
-            </table>
-            <hr />
-          </div>
-        ))}
-      </div>
+                  ) : (
+                    <button
+                      class="hcard decrease"
+                      onClick={() => addToWishlist(prod, setWish)}
+                    >
+                      <span class="material-icons buttonmi">
+                        {" "}
+                        favorite_border{" "}
+                      </span>
+                    </button>
+                  )}
+                </div>
+              </td>
+            </tr>
+          ))}
+        </table>
+      )}
 
       <div className="total-price">
         <h4 style={{ fontWeight: "bold" }}> Invoice </h4>
         <hr />
         <h6> Total Item in cart : {cart.length} </h6>
+        <hr />
+        <h6>Price Items: {totalprice} </h6>
+        <h6> Discount : {totalprice} </h6>
+        <h6> Delivery Charges : {totalprice} </h6>
         <hr />
         <h6> Total Amount : {totalprice} </h6>
       </div>
