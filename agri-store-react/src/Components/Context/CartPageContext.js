a816c2f98b148cbc1c204a4d3da46417bc6c653f
iprankurpandey <pprankur@gmail.com>
2022-05-15 23:11:50 +0530

    feat : implmented reducers

--- agri-store-react/src/Components/Context/CartPageContext.js
+++ agri-store-react/src/Components/Context/CartPageContext.js
@@ -1,22 +1,26 @@
 import axios from "axios";
 import React, { createContext, useState } from "react";
+import { useProductContext } from "./ProductContextReducer";
 export const cartContext = createContext();
 function CartPageContext({ children }) {
-  const [cart, setCart] = useState([]);
+  // const [cart, setCart] = useState([]);
+  const { state, dispatch } = useProductContext();
+  const { cart } = state;
+
   // token
   localStorage.setItem(
     "token",
     `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiJlMzIzZmY2MC1hMTUzLTQ0MTYtYmEyNS0zNDQ0ZGI1NjliOWMiLCJlbWFpbCI6ImFkYXJzaGJhbGlrYUBnbWFpbC5jb20ifQ._-fah2UEuueLmRHHl5uV4CYhiQdODX6neUkGbfTvtFM`
   );
   // add data to cart
-  const addToCart = async (productdata, setCart) => {
+  const addToCart = async (productdata, dispatch) => {
     const response = await axios({
       method: "POST",
       url: `/api/user/cart`,
       headers: { authorization: localStorage.getItem("token") },
       data: { product: productdata },
     });
-    setCart(response.data.cart);
+    dispatch({ type: "CART", payload: response.data.cart });
   };
   // delete data from cart
   async function deleteQty(_id) {
@@ -24,9 +28,9 @@ function CartPageContext({ children }) {
       method: "DELETE",
       url: `/api/user/cart/${_id}`,
       headers: { authorization: localStorage.getItem("token") },
-      data: { product: setCart },
+      data: { product: dispatch },
     });
-    setCart(response.data.cart);
+    dispatch({ type: "CART", payload: response.data.cart });
   }
   // increment /decrement quantity in cart
 
@@ -41,12 +45,13 @@ function CartPageContext({ children }) {
         },
       },
     });
-    setCart(response.data.cart);
+    dispatch({ type: "CART", payload: response.data.cart });
+    // setCart(response.data.cart);
   }
   return (
     <div>
       <cartContext.Provider
-        value={{ updateQty, deleteQty, cart, setCart, addToCart }}
+        value={{ updateQty, deleteQty, cart, dispatch, addToCart }}
       >
         {children}
       </cartContext.Provider>
