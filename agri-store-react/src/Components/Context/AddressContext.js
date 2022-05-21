089118f9ac56f1763b0550ab8d850f685495f8b6
iprankurpandey <pprankur@gmail.com>
2022-05-21 22:21:46 +0530

    fix : address

--- agri-store-react/src/Components/Context/AddressContext.js
+++ agri-store-react/src/Components/Context/AddressContext.js
@@ -1,8 +1,12 @@
-import React, { createContext, useContext, useReducer } from "react";
+import React, { createContext, useContext, useReducer, useState } from "react";
+import { v4 as uuid } from "uuid";
+import Toast from "../Toast/Toast";
 const adressContext = createContext();
 export const useAdressContext = () => useContext(adressContext);
 
 function AddressContext({ children }) {
+  const [alldata, setAlldata] = useState([]);
+  const [toggleSubmit, setToggleSubmit] = useState(false);
   function adresssdataFn(state, action) {
     switch (action.type) {
       case "FULLNAME":
@@ -26,13 +30,48 @@ function AddressContext({ children }) {
     pincode: "",
     address: "",
   });
+  const { id, fullname, email, phone, pincode, address } = state;
 
-  function submitAdress() {
-    dispatch();
+  function formSubmitAddAdress(e) {
+    e.preventDefault();
+    setAlldata([
+      ...alldata,
+      { id: uuid(), fullname, email, phone, pincode, address },
+    ]);
+    Toast({ type: "info", mesg: "Adress added " });
+    dispatch({ type: "FULLNAME", payload: "" });
+    dispatch({ type: "PHONE", payload: "" });
+    dispatch({ type: "EMAIL", payload: "" });
+    dispatch({ type: "PINCODE", payload: "" });
+    dispatch({ type: "ADDRESS", payload: "" });
+  }
+
+  console.log("this is all adress data ", alldata);
+
+  function editAddress(id) {
+    Toast({ type: "info", mesg: "This Feature is coming soon" });
+    console.log("this feature is coming soon");
+  }
+  function saveEditedAddress() {}
+  function deleteAddress(id) {
+    setAlldata(alldata.filter((f) => f.id !== id));
+    Toast({ type: " info", mesg: " Adress Removed " });
+    console.log("delete adress");
   }
   return (
     <div>
-      <adressContext.Provider value={{ state, dispatch, submitAdress }}>
+      <adressContext.Provider
+        value={{
+          state,
+          dispatch,
+          formSubmitAddAdress,
+          editAddress,
+          saveEditedAddress,
+          deleteAddress,
+          alldata,
+          toggleSubmit,
+        }}
+      >
         {children}
       </adressContext.Provider>
     </div>
