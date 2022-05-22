21df2c0b2dd3295a1003ed5020b3a885a374a586
iprankurpandey <pprankur@gmail.com>
2022-05-22 18:21:54 +0530

    fix :  changed usestate to usereducer

--- agri-store-react/src/Components/Context/ProductListingPageContext.js
+++ agri-store-react/src/Components/Context/ProductListingPageContext.js
@@ -1,35 +1,50 @@
 import axios from "axios";
-import React, { createContext, useContext, useEffect, useState } from "react";
+import React, { createContext, useContext, useEffect, useReducer } from "react";
 
 const productDataContext = createContext();
 export const useProductDataContext = () => useContext(productDataContext);
 
 function ProductListingPageContext({ children }) {
-  const [productdata, setProductData] = useState([]);
-  const [isLoading, setisLoading] = useState(false);
+  const [state, dispatch] = useReducer(reducerFn, {
+    productdata: [],
+    isLoading: false,
+  });
+
+  function reducerFn(state, action) {
+    switch (action.type) {
+      case "APIPRODUCTDATA":
+        return { ...state, productdata: action.payload };
+      case "LOADINGSPINNER":
+        return { ...state, isLoading: action.payload };
+      default:
+        return state;
+    }
+  }
+
+  const { productdata } = state;
 
   async function getProdcutsData() {
-    setisLoading(true);
+    dispatch({ type: "LOADINGSPINNER", payload: true });
     try {
       await axios({
         method: "GET",
         url: `/api/products`,
-      }).then((response) => setProductData(response.data.products));
-      setisLoading(false);
+      }).then((response) =>
+        dispatch({ type: "APIPRODUCTDATA", payload: response.data.products })
+      );
+      dispatch({ type: "LOADINGSPINNER", payload: false });
     } catch (erorr) {
       console.log(`Server is encountering some issues:`, erorr);
     }
   }
   // API Call
   useEffect(() => {
-    setisLoading(true);
+    dispatch({ type: "LOADINGSPINNER", payload: true });
     getProdcutsData();
   }, []);
   return (
     <div>
-      <productDataContext.Provider
-        value={{ productdata, setProductData, isLoading }}
-      >
+      <productDataContext.Provider value={{ productdata, state, dispatch }}>
         {children}
       </productDataContext.Provider>
     </div>
